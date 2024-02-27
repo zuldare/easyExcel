b97bea939d00c166fbaaea6ee2ef5557f24a3960
Jaime Hernandez <jaime.hernandez@mirai-advisory.com>
2024-02-27 17:37:15 +0100

    Frist commit

---
+++ src/test/java/com/jh/EasyExcel/EasyExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.EasyExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
